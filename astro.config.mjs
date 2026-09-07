bee5132aac325a3a5075e7f8a92efdb459a5fb59
l.heywang <leonard.heywang@proton.me>
2026-09-07 22:59:23 +0200

    Added the missing articles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
 
     site: 'https://https://home-hardware.app/',
     // base: '/home-hardware',
+
+    markdown: {
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
+    },
 
 	integrations: [
 		starlight({
@@ -20,6 +27,7 @@ export default defineConfig({
                 },
             },
 
+            
 			social: [
                 { icon: 'github', label: 'GitHub', href: 'https://github.com/lheywang/home-hardware' }, 
                 {icon: 'discord', label: 'Discord', href: 'https://discord.gg/R9HNVnBs6s'}
@@ -27,6 +35,7 @@ export default defineConfig({
 
             customCss: [
                 './src/styles/custom.css',
+                'katex/dist/katex.min.css'
             ],
 
 			sidebar: [
